57d4e90c2d95ea154314769e1992c120a6f6be45
jjmattos1 <brainstorm@jonasmattos.com.br>
2025-02-18 21:41:17 -0300

    begin...

---
+++ projeto_inicial1/bin/projeto_inicial1.dart
@@ -0,0 +1,7 @@
+import 'package:projeto_inicial1/projeto_inicial1.dart' as projeto_inicial1;
+
+void main() {
+  print('Hello world: ${projeto_inicial1.calculate()}!');
+  // projeto_inicial1 devido ao primeiro projeto ter bugado, de alguma forma, o
+  // VS Code a ponto dele congelar a tela toda vez que o projeto era carregado
+}
