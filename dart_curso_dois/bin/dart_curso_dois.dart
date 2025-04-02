0bf3a63522587d4941aed0fc7b7dbe6785c87f93
jjmattos1 <brainstorm@jonasmattos.com.br>
2025-04-01 22:36:23 -0300

    06 Faça como eu fiz: Polimorfismo e Sobrescrita.. falta ajustar uma ideia que comentei.

--- dart_curso_dois/bin/dart_curso_dois.dart
+++ dart_curso_dois/bin/dart_curso_dois.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   //print('Olá mundo!\n\n');
   // Paradigmas de linguagem de programação: https://blog.geekhunter.com.br/quais-sao-os-paradigmas-de-programacao/
-  
+
   String nome = "Laranja";
   double peso = 100.2;
   String cor = "Verde e Amarela";
@@ -19,7 +19,7 @@ void main() {
   // Clean code, DRY. Dont Repeat Yourself. Evite repetir código!
   // Pensar sempre em criar funções caixa preta, para que elas mesmas resolvam da melhor forma o problema
   // Exemplo de função caixa preta:
-/*   int idade;
+  /*   int idade;
   if (idade >= 18) {
     return true;
   } else {
@@ -27,7 +27,7 @@ void main() {
   } */
 
   // colocar a palavra void antes de criar a função é algo opcional, pois mesmo não colocando, o Dart entende de que aquela função é void
-  
+
   // Sobre argumentos
   // 1 - Posicionais obrigatórios = obedecem a ordem como os argumentos são passados
   // 2 - Nomeados opcionais = para usar, se coloca chaves {} entre os argumentos opcionais
@@ -35,153 +35,223 @@ void main() {
   // 4 - Modificador "required"
   // d
 
-    //mostrarMadura("Uva", 40, cor: "Roxa");
+  //mostrarMadura("Uva", 40, cor: "Roxa");
 
-    // cont...
+  // cont...
 
-/*     int quantosDias = funcQuantosDiasMadura(diasDesdeColheita);
+  /*     int quantosDias = funcQuantosDiasMadura(diasDesdeColheita);
     print("Dias para amadurecer ");
     print(quantosDias);
 
     print("\n\n"); */
 
-    /* print("toString method... ");
+  /* print("toString method... ");
     print(toString(nome: "Laranja", peso: 100.2, diasDesdeColheita: 20));
 
     print("\n\nshowFruitInfo method...");
     print(showFruitInfo(nome: "Laranja", peso: 100.2, diasDesdeColheita: 20, diasAmadurecer: 30)); */
-    
-    // começo sobre classes no Dart
 
-    Fruta fruta01 = Fruta(nome, peso, cor, sabor, diasDesdeColheita);
-    //Fruta fruta02 = Fruta.nomeada(nome: "Uva", peso: 60.5, cor: "Roxa", sabor: "Doce", diasDesdeColheita: 40);
+  // começo sobre classes no Dart
 
-    print(showFruitInfo(nome: "Banana", peso: 150, diasDesdeColheita: 20, diasAmadurecer: 30));
-    print("");
-    print("${fruta01.nome} é $fruta01");
-    print("");
-    // print("${fruta02.cor} é $fruta02");
-    // print(fruta02.estaMadura(20));
+  Fruta fruta01 = Fruta(nome, peso, cor, sabor, diasDesdeColheita);
+  //Fruta fruta02 = Fruta.nomeada(nome: "Uva", peso: 60.5, cor: "Roxa", sabor: "Doce", diasDesdeColheita: 40);
 
-    Alimento mandioca1 = Alimento("mandioca", "marrom", 500);
-    print('${mandioca1.nome} text ${mandioca1.cor}');
-    // print(fruta02.cor);
-    // print(fruta02);  
-    print("");
-    //print(fruta01.estaMadura);
+  // print(showFruitInfo(nome: "Banana", peso: 150, diasDesdeColheita: 20, diasAmadurecer: 30));
+  // print("");
+  // print("${fruta01.nome} é $fruta01");
+  // print("");
+  // print("${fruta02.cor} é $fruta02");
+  // print(fruta02.estaMadura(20));
 
-    print("");
-    Legumes mandioca2 = Legumes("Macaxeira", 1200, "Marrom", true);
-    Fruta banana2 = Fruta("Banana", 120, "Amarela", "Doce", 15);
-    Nozes macadamia1 = Nozes("Macadâmia", 2, 'Branco Amarelado', 'Doce', 20, 35);
-    Citricas limao1 = Citricas('Limão', 5, "Verde", 'Azedo', 5, 9);
+  Alimento mandioca1 = Alimento("mandioca", "marrom", 500);
+  // print('${mandioca1.nome} text ${mandioca1.cor}');
+  // print(fruta02.cor);
 
-    mandioca2.printAlimento();
-    banana2.printAlimento();
-    macadamia1.printAlimento();
-    limao1.printAlimento();
- 
-    Citricas limao2 = Citricas.nomeada(nome: "Limão", peso: 5, cor: "Verde", sabor: "Azedo", diasDesdeColheita: 5, nivelAzedo: 9);
-    
-    limao2.printAlimento();
-    mandioca2.cozinhar();
-    limao1.fazerSuco();
+  Legumes mandioca2 = Legumes("Macaxeira", 1200, "Marrom", true);
+  Fruta banana2 = Fruta("Banana", 120, "Amarela", "Doce", 15);
+  Nozes macadamia1 = Nozes("Macadâmia", 2, 'Branco Amarelado', 'Doce', 20, 35);
+  Citricas limao1 = Citricas('Limão', 5, "Verde", 'Azedo', 5, 9);
 
+  // mandioca2.printAlimento();
+  // banana2.printAlimento();
+  // macadamia1.printAlimento();
+  // limao1.printAlimento();
 
-    // usando o extends na classe, permite você configurar ela para receber os parametros de outra classe como herança
-    // desta forma, para inicializar o constutor desa classe, basta adicionar ao final do metodo
-    // construtor o codigo : super(argumetnos que inicializam o construtor em questão passar aqui no parenteses).
-    // E, lembrando, de colocar o tipo da variável no argumento do construtor, antes do super.
+  Citricas limao2 = Citricas.nomeada(
+    nome: "Limão",
+    peso: 5,
+    cor: "Verde",
+    sabor: "Azedo",
+    diasDesdeColheita: 5,
+    nivelAzedo: 9,
+  );
 
-    // para deixar varias linhas selecionadas como comentario, apertar ctrl+k+c!
-    // EOF
-    // relembrando.. shift+tab remove a identação na linha, ou nas linhas selecionadas. E pressionando somente o tab
-    // coloca um ponto de indentação na dada linha(s) selecionada(s).
+  banana2.separarIngredientes();
+  banana2.printAlimento();
+  // multi line string test
+  print("Pensar em uma maneira de herdar os metodos da classe Bolo, na classe fruta."
+   """
+ E não o inverso como acontece atualmente!
+Nova linha aqui
+ """);
+  // banana2.fazerMassa();
+  // banana2.assar();
+
+  // escrever testes com classses abstratas e polimorfismo
+
+  // usando o extends na classe, permite você configurar ela para receber os parametros de outra classe como herança
+  // desta forma, para inicializar o constutor desa classe, basta adicionar ao final do metodo
+  // construtor o codigo : super(argumetnos que inicializam o construtor em questão passar aqui no parenteses).
+  // E, lembrando, de colocar o tipo da variável no argumento do construtor, antes do super.
+
+  // para deixar varias linhas selecionadas como comentario, apertar ctrl+k+c!
+  // EOF
+  // relembrando.. shift+tab remove a identação na linha, ou nas linhas selecionadas. E pressionando somente o tab
+  // coloca um ponto de indentação na dada linha(s) selecionada(s).
+  // Auto indentação vs code Windows é 'shift + alt + f". No Ubuntu a combinação é "ctr + shit + I".
 }
 
-    class Alimento {
-      String nome;
-      double peso;
-      String cor;
-      Alimento(this.nome,this.cor,this.peso);
-
-      void printAlimento(){
-        print('Este(a) $nome pesa $peso e é $cor.');
-      }
-    }
-
-    class Fruta extends Alimento {
-      String sabor;
-      int diasDesdeColheita;
-      bool? isMadura;
-      estaMadura(int diasParaMadura) {
-        isMadura = diasDesdeColheita >= diasParaMadura;
-        print("A fruta $nome foi colhida a $diasDesdeColheita dias. Como ela precisa de $diasParaMadura dias para amadurecer, ela está madura? $isMadura.");
-      }
-
-  Fruta(String nome, double peso, String cor, this.sabor, this.diasDesdeColheita, {this.isMadura}):super(nome,cor,peso);
-
-  void fazerSuco(){
-        print('Você fez um ótimo suco $nome');
-      }
-
+abstract class Bolo implements Alimento {
+  // int? tempoCozimentoBolo;
+  void separarIngredientes() {
+    print("Separar a(o) $nome ($peso gramas), farinha, leite, ovos e açucar!\n");
+  }
+  void fazerMassa() {
+    print("Misturar os ingredientes até todos ficarem homogêneos!");
+  }
+  void assar() {
+    print("Depois da massa estar homogenea, levar ao forno por $tempoCozimentoBolo minutos!");
+  }
+  
 }
 
-      // Fruta.nomeada({required this.nome, required this.peso, required this.cor, required this.sabor, required this.diasDesdeColheita});
+class Alimento {
+  String nome;
+  double peso;
+  String cor;
+  int? tempoCozimentoBolo;
+  Alimento(this.nome, this.cor, this.peso);
 
-  /*  Fruta.minusculas(this.nome, this.cor) {
+  void printAlimento() {
+    print('Este(a) $nome pesa $peso gramas e é $cor.');
+  }
+}
+
+class Fruta extends Alimento implements Bolo {
+  String sabor;
+  int diasDesdeColheita;
+  bool? isMadura;
+  estaMadura(int diasParaMadura) {
+    isMadura = diasDesdeColheita >= diasParaMadura;
+    print(
+      "A fruta $nome foi colhida a $diasDesdeColheita dias. Como ela precisa de $diasParaMadura dias para amadurecer, ela está madura? $isMadura.",
+    );
+  }
+
+  Fruta(
+    String nome,
+    double peso,
+    String cor,
+    this.sabor,
+    this.diasDesdeColheita, {
+    this.isMadura,
+  }) : super(nome, cor, peso);
+
+  void fazerSuco() {
+    print('Você fez um ótimo suco $nome');
+  }
+  
+  // @override
+  // int? tempoCozimentoBolo;
+  
+  @override
+  void assar() {
+    // TODO: implement assar
+  }
+  
+  @override
+  void fazerMassa() {
+    // TODO: implement fazerMassa
+  }
+  
+  @override
+  void separarIngredientes() {
+    // TODO: implement separarIngredientes
+  }
+}
+
+// Fruta.nomeada({required this.nome, required this.peso, required this.cor, required this.sabor, required this.diasDesdeColheita});
+
+/*  Fruta.minusculas(this.nome, this.cor) {
     nome = nome.toLowerCase();
     cor = cor.toLowerCase();
     } */
-  // finalizando testes aula 03...
+// finalizando testes aula 03...
 
+class Citricas extends Fruta {
+  double nivelAzedo;
 
+  Citricas(
+    String nome,
+    double peso,
+    String cor,
+    String sabor,
+    int diasDesdeColheita,
+    this.nivelAzedo,
+  ) : super(nome, peso, cor, sabor, diasDesdeColheita);
 
-    class Citricas extends Fruta {
-      double nivelAzedo;
-      
-      Citricas(String nome, double peso, String cor, String sabor, int diasDesdeColheita, this.nivelAzedo)
-        :super(nome, peso, cor, sabor, diasDesdeColheita);
-      
-      Citricas.nomeada({required String nome, required double peso, required String cor, required String sabor, required int diasDesdeColheita, required this.nivelAzedo})
-      :super(nome, peso, cor, sabor, diasDesdeColheita);
-      
-      void existeRefri(bool existe){
-        if(existe){
-          print('Existe refrigerante de $nome');
-        }else{
-          print('Não existe refri de $nome');
-        }
-      }
-    }
-
-    class Nozes extends Fruta {
-      double porcentagemOleoNatural;
-      Nozes(String nome, double peso, String cor, String sabor,int diasDesdeColheita, this.porcentagemOleoNatural)
-        :super(nome, peso, cor, sabor, diasDesdeColheita);
-    }
-
-    class Legumes extends Alimento {
-      bool isPrecisaCozinhar;
-      //double peso;
-      Legumes(String nome, double peso, String cor, this.isPrecisaCozinhar):super(nome,cor,peso);
-
-      void cozinhar() {
-        if(isPrecisaCozinhar){
-          print('Pronto, o $nome está cozinhando!');
-        }else{
-          print('Nem precisa cozinhar!');
-        }
-      }
-
-    }
-
-String toString(
-    {required String nome,
+  Citricas.nomeada({
+    required String nome,
     required double peso,
-    required diasDesdeColheita,
-    int diasParaMadura = 30,
-    bool? isMadura}) {
+    required String cor,
+    required String sabor,
+    required int diasDesdeColheita,
+    required this.nivelAzedo,
+  }) : super(nome, peso, cor, sabor, diasDesdeColheita);
+
+  void existeRefri(bool existe) {
+    if (existe) {
+      print('Existe refrigerante de $nome');
+    } else {
+      print('Não existe refri de $nome');
+    }
+  }
+}
+
+class Nozes extends Fruta {
+  double porcentagemOleoNatural;
+  Nozes(
+    String nome,
+    double peso,
+    String cor,
+    String sabor,
+    int diasDesdeColheita,
+    this.porcentagemOleoNatural,
+  ) : super(nome, peso, cor, sabor, diasDesdeColheita);
+}
+
+class Legumes extends Alimento {
+  bool isPrecisaCozinhar;
+  //double peso;
+  Legumes(String nome, double peso, String cor, this.isPrecisaCozinhar)
+    : super(nome, cor, peso);
+
+  void cozinhar() {
+    if (isPrecisaCozinhar) {
+      print('Pronto, o $nome está cozinhando!');
+    } else {
+      print('Nem precisa cozinhar!');
+    }
+  }
+}
+
+String toString({
+  required String nome,
+  required double peso,
+  required diasDesdeColheita,
+  int diasParaMadura = 30,
+  bool? isMadura,
+}) {
   if (isMadura == null) {
     isMadura = diasDesdeColheita >= diasParaMadura;
   }
@@ -198,33 +268,38 @@ String toString(
 }
 
 // A Laranja pesa 98 gramas! Ela foi colhida há 30 dias e precisa de 20 para amadurecer, logo, a Laranja está madura!
-String showFruitInfo({required nome, required double peso, required int diasDesdeColheita, required int diasAmadurecer}){
+String showFruitInfo({
+  required nome,
+  required double peso,
+  required int diasDesdeColheita,
+  required int diasAmadurecer,
+}) {
   bool isMadura;
   isMadura = diasDesdeColheita >= diasAmadurecer;
-  
-  String msg = "A $nome pesa $peso! Ela foi colhida há $diasDesdeColheita e precisa de $diasAmadurecer para amadurecer, logo, a $nome está madura? $isMadura";
-  
+
+  String msg =
+      "A $nome pesa $peso! Ela foi colhida há $diasDesdeColheita e precisa de $diasAmadurecer para amadurecer, logo, a $nome está madura? $isMadura";
+
   return msg;
 }
 
-int funcQuantosDiasMadura(int dias){
-    int diasParaMadura = 30;
-    int quantosDiasFaltam = diasParaMadura - dias;
-    return quantosDiasFaltam;
+int funcQuantosDiasMadura(int dias) {
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = diasParaMadura - dias;
+  return quantosDiasFaltam;
 }
 
 mostrarMadura(String nome, int dias, {required String cor}) {
-    if (dias >= 30) {
-      print("A $nome está madura.");
-    }
-    else {
-      print("A $nome não está madura.");
-    }
-
-    if (cor != null) {
-      print("A $nome é $cor.");
-    }
+  if (dias >= 30) {
+    print("A $nome está madura.");
+  } else {
+    print("A $nome não está madura.");
   }
+
+  if (cor != null) {
+    print("A $nome é $cor.");
+  }
+}
 
 bool funcEstaMadura(int dias) {
   if (dias >= 30) {
